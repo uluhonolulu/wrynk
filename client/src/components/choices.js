f7a4eb42685bb73019baab8a48e72aa6ba48715a
Artem Smirnov <uluhonolulu@gmail.com>
2018-05-26 17:47:46 +0300

    Displaying choices statically

--- client/src/components/choices.js
+++ client/src/components/choices.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
 
 export default class Choices extends Component {
+
+  state = {
+    choices: [
+      {
+        "$class": "org.rynk.Choice",
+        "name": "Dobro"
+      }
+    ],
+    results: [
+      {
+        "$class": "org.rynk.VoteTotal",
+        "choiceName": "Dobro",
+        "votedChoice": "resource:org.rynk.Choice#Dobro",
+        "count": 1
+      }
+    ],
+    canVote: false
+  }
+
   render() {
+    const choices = this.state.choices.map((choice) => {
+      return (
+        <div>{choice.name}</div>
+      );
+    })
     return (
       <div>
-        Choices
+        {choices}
       </div>
     )
   }
