4d9367247224cbf48174d6e26fa089a4ff921d7b
Artem Smirnov <uluhonolulu@gmail.com>
2018-05-26 15:23:41 +0300

    Bootstrapped the client

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,34 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <header>
+          <nav className="navbar navbar-inverse navbar-static-top" role="navigation">
+            <div className="container">
+              <div className="navbar-header">
+                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                  <span className="sr-only">Toggle navigation</span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                </button>
+                <a className="navbar-brand" href="#">rynk</a>
+              </div>
+              <div id="navbar" className="navbar-collapse collapse navbar-right">
+                <a href="/auth/github" className="btn btn-success">Sign in</a>
+              </div>
+            </div>
+          </nav>
+
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+
+        <div className="container">
+          <div className="row">
+
+          </div>
+        </div>
+
       </div>
     );
   }
