4ab3affcdad6865c44c5623d2b0f21aa11c09bba
Artem Smirnov <uluhonolulu@gmail.com>
2018-06-02 14:39:33 +0300

    Loader; more error handling.

--- client/src/components/choices.js
+++ client/src/components/choices.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 import { Button, FormGroup, Radio, Alert } from 'react-bootstrap';
 import cookie from 'react-cookies';
 import uuidv1 from 'uuid/v1';
+import Loader from 'react-loader-spinner';
 
 
 export default class Choices extends Component {
   constructor(props, context) {
     super(props, context);
 
-    this.handleClick = this.handleClick.bind(this);
+    // this.handleClick = this.handleClick.bind(this);
 
     this.state = {
       choices: [],
@@ -20,7 +21,6 @@ export default class Choices extends Component {
           "count": 1
         }
       ],
-      canVote: false,
       isLoading: true,
       access_token: cookie.load('access_token')
     };
@@ -29,43 +29,45 @@ export default class Choices extends Component {
   }
 
 
-  async componentWillMount() {
+  async componentDidMount() {
     if (!this.state.access_token) {
       this.setState({ isLoading: false });
       return;
     }
-    const canVote = await this.canIVote();
-    this.setState({ canVote });
-    const response = await fetch(`/api/Choice?access_token=${this.state.access_token}`);
-    if(response.status === 200) {
-      const choices = await response.json();
-      this.setState({ choices });
-    } else {
-      await this.handleInvalidResponse(response); 
+
+    try {
+      const canVote = await this.canIVote();
+      this.setState({ canVote });
+      // const response = await fetch(`/api/Choice?access_token=${this.state.access_token}`).then(this.handleResponse.bind(this));
+      const choices = await this.getChoices();
+      this.setState({ choices });    
+    } catch (error) {
+      //everything's handled already
     }
+
     
     this.setState({ isLoading: false }); 
   }  
 
   async canIVote() {
     try {
-      const response = await fetch(`/api/CanVote?access_token=${this.state.access_token}`, {method: "post"});
-      console.log(response.status);  
-      console.log(response.statusText);  
-      console.log(response.type);       
-      if (response.status === 200) {
-        return true;  
-      } else {
-        await this.handleInvalidResponse(response);
-        return false;
-      }
+      const response = await this.callBlockchain('CanVote');     
+      return true;      //if this transaction doesn't throw, it means we can vote; 
     } catch (error) {
-      console.log(error);
-      return false;
+      //TODO: check for error text
+      return false;      
     }
   }
 
+  async getChoices(){
+    const response = await this.callBlockchain('Choice');
+    const choices = await response.json();
+    return choices;
+  }
+
   render() {
+
+
     if (!this.state.access_token) {
       // window.location.assign("/auth/github");
       return (<Alert bsStyle="danger">Please <a href="/auth/github">sign in</a>!</Alert>);
@@ -73,6 +75,10 @@ export default class Choices extends Component {
 	
     if (this.state.error) {
         return (<Alert bsStyle="danger">{this.state.error.message}</Alert>);		
+    }
+
+    if (this.state.isLoading) {
+      return (<Loader type="TailSpin"/>);      
     }
 
     const canVote = this.state.canVote;
@@ -87,7 +93,7 @@ export default class Choices extends Component {
     return (
       <form onSubmit={ this.onFormSubmit }>
       <FormGroup>
-        {canVote? null : (cannotVoteMessage)}
+        {(canVote !== false)? null : (cannotVoteMessage)}
         {choices}
         <Button bsStyle="success" disabled={isLoading} type="submit">    
           {isLoading ? 'Please wait...' : 'Vote'}
@@ -137,18 +143,50 @@ export default class Choices extends Component {
     }        
   }
 
-  async handleClick() {
+  async callBlockchain(name, method, transactionData){
+    let requestData;
+    if (method === "post") {
+      requestData = {
+        method: "post",  
+        headers: {  
+          "content-type": "application/json",
+          "Accept": "application/json"  
+        }
+      };
+      if (transactionData) {
+        requestData.body = JSON.stringify(transactionData);
+      }
+    }
 
+    let url = `/api/${name}?access_token=${this.state.access_token}`;
+
+    return await fetch(url, requestData).then(this.handleResponse.bind(this)); 
+  }
+
+  async handleResponse(response){
+    if (response.ok) {
+      return response;
+    } else {
+      let message = await this.handleInvalidResponse(response);
+      this.setState({error: {message}});
+      throw new Error(message);    //so that we don't proceed
+    }
   }
 
   async handleInvalidResponse(response) {
     if (response.status === 404) {
-      this.setState({error: {message: "Blockchain is not started."}});
+      return "Blockchain is not started.";
     } else if (response.status === 401) {
-      this.setState({error: {message: "Please <a href='/auth/github'>sign in</a>!"}});
+      return "Please <a href='/auth/github'>sign in</a>!";
     } else {
-      const error = await response.json();
-      this.setState({ error: error.error });      
+      let contentType = response.headers.get("Content-Type");
+      if (contentType && contentType.includes('json')) {
+        const error = await response.json();
+        return error.error.message;     
+      } else {
+        const responseText = await response.text();
+        return responseText;
+      }     
     }
   }
   
